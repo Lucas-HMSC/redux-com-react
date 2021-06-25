53aab8ba52cf0c502e32baa5548e274053ee5dab
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-06-25 13:00:51 -0300

    Aula 0408 - Desafio

--- Aula 0400 - React Redux/react-app/src/Components/Content.js
+++ Aula 0400 - React Redux/react-app/src/Components/Content.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 import Login from './Login';
+import Photos from './Photos';
+import Loading from './Helper/Loading';
 
 const Content = () => {
-  return (
-    <div>
-      <Login />
-    </div>
-  )
+  const { user, token } = useSelector(state => state.login);
+
+  if (user.loading || token.loading)
+    return <Loading /> 
+  if (user.data)  
+    return <Photos />
+  else
+    return <Login />
 }
 
 export default Content
