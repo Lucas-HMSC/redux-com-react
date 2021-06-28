bea802aa7873265f47d0db0aa960d6b27d9df914
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-06-28 12:52:43 -0300

    Aula 0408 - Desafio

--- Aula 0400 - React Redux/react-app/src/Components/Login.js
+++ Aula 0400 - React Redux/react-app/src/Components/Login.js
@@ -15,7 +15,7 @@ const Login = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form className='anime' onSubmit={handleSubmit}>
       <label className={styles.label} htmlFor="username">
         Usuário
       </label>
