a7c95938da8759d68c2a1317f687ce18c134e56b
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-06-30 12:51:28 -0300

    Aula 0503 - Cache

---
+++ Aula 0503 - Cache/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Photos from './Photos';
+
+function App() {
+  const [toggle, setToggle] = React.useState(true);
+
+  return (
+    <div>
+      <button
+        onClick={() => setToggle(!toggle)}
+      >
+        Toggle
+      </button>
+      { toggle && <Photos /> }
+    </div>
+  );
+}
+
+export default App;
