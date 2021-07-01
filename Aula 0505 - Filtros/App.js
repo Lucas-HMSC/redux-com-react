23185733a5b862e7c857a2b75fd050aafd9cc646
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-07-01 13:30:55 -0300

    Aula 0505 - Filtros

---
+++ Aula 0505 - Filtros/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Filter from './Filter';
+import Products from './Products';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Filter />
+      <Products />
+    </div>
+  );
+}
+
+export default App;
