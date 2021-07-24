6167374f9131a2b6b31bcc94a939828cd584d69b
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-07-24 10:57:18 -0300

    Redux Project - Modal

--- Aula 0600 - Projeto Redux/src/Components/Feed/FeedModal.js
+++ Aula 0600 - Projeto Redux/src/Components/Feed/FeedModal.js
@@ -3,21 +3,24 @@ import { useSelector, useDispatch } from 'react-redux';
 import styles from './FeedModal.module.css';
 import Error from '../Helper/Error';
 import Loading from '../Helper/Loading';
-import { PHOTO_GET } from '../../Api';
 import PhotoContent from '../Photo/PhotoContent';
-import { fetchPhoto } from '../../store/photo';
+import { closeModal } from '../../store/ui';
 
-const FeedModal = ({ photo, setModalPhoto }) => {
+const FeedModal = () => {
+  const { modal } = useSelector(state => state.ui);
+
   const { loading, error, data } = useSelector(state => state.photo);
   const dispatch = useDispatch();
 
-  React.useEffect(() => {
-    dispatch(fetchPhoto(photo.id));
-  }, [dispatch, photo.id]);
-
   function handleOutsideClick(event) {
-    if (event.target === event.currentTarget) setModalPhoto(null);
+    if (event.target === event.currentTarget) dispatch(closeModal());
   }
+
+  React.useEffect(() => {
+    dispatch(closeModal());
+  }, [dispatch]);
+
+  if (!modal) return null;
 
   return (
     <div className={styles.modal} onClick={handleOutsideClick}>
--- Aula 0600 - Projeto Redux/src/Components/Feed/FeedPhotosItem.js
+++ Aula 0600 - Projeto Redux/src/Components/Feed/FeedPhotosItem.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import styles from './FeedPhotosItem.module.css';
+import { useDispatch } from 'react-redux';
 import Image from '../Helper/Image';
+import { openModal } from '../../store/ui';
+import { fetchPhoto } from '../../store/photo';
+import styles from './FeedPhotosItem.module.css';
 
-const FeedPhotosItem = ({ photo, setModalPhoto }) => {
+const FeedPhotosItem = ({ photo }) => {
+  const dispatch = useDispatch();
+
   function handleClick() {
-    setModalPhoto(photo);
+    dispatch(openModal());
+    dispatch(fetchPhoto(photo.id));
   }
   return (
     <li className={styles.photo} onClick={handleClick}>
